379e0bdd50e7a8a5f085eec888aaebd10646cc44
sansanthosh0000 <sansanthosh0000@gmail.com>
2021-07-04 21:50:42 +0530

    3rd

---
+++ lenght.java
@@ -0,0 +1,22 @@
+package one;
+import java.lang.*;
+
+public class lenght {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a ="hello java";
+public void len (char a)
+{	
+	System.out.println();
+}
+		
+		//lenght s = new lenght();
+	//String b = a
+	
+		
+		System.out.println(a.);
+	}
+
+}
